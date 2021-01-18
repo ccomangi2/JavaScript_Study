92258bcb7085d36633004ce853ff561da32dd696
Lee Su Bin <ccomangi2@gmail.com>
2021-01-19 00:25:27 +0900

    2021.01.19 am 12:25

---
+++ part1. 코어 자바스크립트/01. 자바스크립트 기본/05. 형 변환.js
@@ -0,0 +1,18 @@
+let value = true;
+alert(typeof value); // boolean
+
+value = String(value); // 변수 value엔 문자열 "true"가 저장됩니다.
+alert(typeof value); // string
+
+//
+let str = "123";
+alert(typeof str); // string
+
+let num = Number(str); // 문자열 "123"이 숫자 123으로 변환됩니다.
+
+alert(typeof num); // number
+
+//
+let age = Number("임의의 문자열 123");
+
+alert(age); // NaN, 형 변환이 실패합니다.
